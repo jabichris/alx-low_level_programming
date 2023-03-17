cd3a685cb521177d81c0f3712d54c664dacb6bf9
jabichris <seannas04@gmail.com>
2023-03-17 14:13:45 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,16 +6,12 @@
  */
 int main(void)
 {
-	putchar('0');
-	putchar('1');
-	putchar('2');
-	putchar('3');
-	putchar('4');
-	putchar('5');
-	putchar('6');
-	putchar('7');
-	putchar('8');
-	putchar('9');
+	int dig;
+
+	for (dig = 0; dig < 10; dig++)
+	{
+		putchar(dig);
+	}
 	putchar('\n');
 	return (0);
 }
