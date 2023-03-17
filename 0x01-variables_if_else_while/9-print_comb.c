4f8c6933aa675d120fc96bdc5beeade718f42b56
jabichris <seannas04@gmail.com>
2023-03-17 14:46:35 -0700

    use 4 putchar

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,22 +6,17 @@
  */
 int main(void)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
-		{
-			putchar(i + '0');
-			putchar(',');
-			putchar(' ');
-			putchar(j + '0');
-			if (i != 9 || j != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
+		putchar(i + '0');
+
+		if (i == 9)
+			break;
+
+		putchar(',');
+		putchar(' ');
 	}
 
 	putchar('\n');
