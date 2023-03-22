9678fd347bffdbe7697d6ebf86ad850f334be77d
jabichris <seannas04@gmail.com>
2023-03-22 13:22:39 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - main function
+ *
+ * @num: integer to make absolute
+ * Return: int or negative int
+ *
+ */
+int _abs(int num)
+{
+	if (num < 0)
+		return (-num);
+	return (num);
+}
