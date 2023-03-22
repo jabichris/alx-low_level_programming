9ba5f28b54beb37a21217afbf4246b031e4589a9
jabichris <seannas04@gmail.com>
2023-03-22 14:03:22 -0700

    8-24_hours.cf

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * jack_bauer - print.
- * 
+ *
 */
 void jack_bauer(void)
 {
@@ -9,7 +9,12 @@ void jack_bauer(void)
 	{
 		for (int minute = 0; minute < 60; minute++)
 		{
-			_putchar("%02d:%02d\n", hour, minute);
+			_putchar(hour / 10 + '0');
+			_putchar(hour % 10 + '0');
+			_putchar(':');
+			_putchar(minute / 10 + '0');
+			_putchar(minute % 10 + '0');
+			_putchar('\n');
 		}
 	}
 }
